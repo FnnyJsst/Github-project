51d5099cd68815303fc2979344670711dded1850
FnnyJsst <fanny.jousset@gmail.com>
2023-12-24 09:19:46 +0100

    code simplified

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-const usernames = ['user1', 'user2', 'user3']; // Add the GitHub usernames you want to fetch
+const usernames = ['user1', 'user2', 'user3'];
 
 const fetchUserInformation = async (username) => {
     const response = await fetch(`https://api.github.com/users/${username}`);
-    if (!response.ok) {
-        console.error(`Error fetching data for ${username}: ${response.statusText}`);
-        return null;
-    }
-    const data = await response.json();
-    return data;
+    if (!response.ok) throw new Error('Error');
+    return response.json();
 };
 
-const fetchAllUserInformation = async () => {
-    const userPromises = usernames.map(username => fetchUserInformation(username));
-    const userData = await Promise.all(userPromises);
-    return userData;
-};
+const fetchAllUserInformation = async () => Promise.all(usernames.map(fetchUserInformation));
 
 fetchAllUserInformation()
-    .then(users => {
-        console.log(users);
-    })
-    .catch(error => {
-        console.error('Error fetching user information:', error);
-    });
+    .then(users => console.log(users))
+    .catch(error => console.error('Error fetching user information:', error));
+
+
+function researchUser() {
+    
+}
