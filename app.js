a15762882de15b3b9ce7a8ef190a51e13be80dbd
FnnyJsst <fanny.jousset@gmail.com>
2023-12-24 12:33:43 +0100

    display github username when searched in form

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-const usernames = ['user1', 'user2', 'user3'];
+const usernames = [];
+const textarea = document.getElementById("textarea");
+const researchButton = document.getElementById('research-button');
 
 const fetchUserInformation = async (username) => {
     const response = await fetch(`https://api.github.com/users/${username}`);
-    if (!response.ok) throw new Error('Error');
+    if (!response.ok) {
+        console.error(`Error fetching user information for ${username}:`, response.status);
+        throw new Error('Error');
+    }
     return response.json();
 };
 
-const fetchAllUserInformation = async () => Promise.all(usernames.map(fetchUserInformation));
+// Fetch all usernames from GitHub
+const fetchAllUsernames = async () => {
+    const response = await fetch(`https://api.github.com/users?per_page=100`);
+    if (!response.ok) throw new Error('Error');
+    const data = await response.json();
 
-fetchAllUserInformation()
-    .then(users => console.log(users))
-    .catch(error => console.error('Error fetching user information:', error));
+    for (const user of data) {
+        usernames.push(user.login);
+        usernames.push(user.avatar_url);
+    }
+};
 
+fetchAllUsernames()
+    .then(() => {
+        console.log(usernames);
+    })
+    .catch(error => console.error('Error fetching usernames:', error));
 
-function researchUser() {
+    async function researchUser() {
+        const text = textarea.value;
     
-}
+        for (const username of usernames) {
+            if (text.includes(username)) {
+                try {
+                    const user = await fetchUserInformation(username);
+                    let userInfo = document.getElementById('user-info');
+                    userInfo.textContent = `${user.login}`;
+                    
+
+                break;
+                } catch (error) {
+                    console.error('Error fetching user information:', error);
+                }
+            }
+        }
+    }
+    
+
+researchButton.addEventListener('click', researchUser);
+
