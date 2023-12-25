65a81799c7ada196fbfb539e5fe103cba608211b
FnnyJsst <fanny.jousset@gmail.com>
2023-12-25 10:19:58 +0100

    simplification du code

--- app.js
+++ app.js
@@ -1,56 +1,48 @@
-const usernames = [];
+//Déclaration des variables
+const userInformation = {};
 const textarea = document.getElementById("textarea");
 const researchButton = document.getElementById('research-button');
+const resultsContainer = document.getElementById('results-container');
 
-const fetchUserInformation = async (username) => {
-    const response = await fetch(`https://api.github.com/users/${username}`);
-    if (!response.ok) {
-        console.error(`Error fetching user information for ${username}:`, response.status);
-        throw new Error('Error');
-    }
-    return response.json();
-};
-
-// Fetch all usernames from GitHub
-const fetchAllUsernames = async () => {
+//Fetch Github API
+const fetchAllUserInformation = async () => {
     const response = await fetch(`https://api.github.com/users?per_page=100`);
     if (!response.ok) throw new Error('Error');
     const data = await response.json();
 
+//Crée un objet avec le pseudo Github en clé et l'avatar en valeur associée
     for (const user of data) {
-        usernames.push(user.login);
-        usernames.push(user.avatar_url);
+        userInformation[user.login] = {
+            avatar_url: user.avatar_url
+        };
     }
 };
 
-fetchAllUsernames()
-    .then(() => {
-        console.log(usernames);
-    })
-    .catch(error => console.error('Error fetching usernames:', error));
 
-    async function researchUser() {
-        const text = textarea.value;
-    
-        for (const username of usernames) {
-            if (text.includes(username)) {
-                try {
-                    const user = await fetchUserInformation(username);
-                    let userInfo = document.getElementById('user-info');
-                    userInfo.textContent = `${user.login}`;
-                    userInfo.innerHTML = 
-                    `<h1> ${user.login}</h1>
-                    <img src=${user.avatar_url}</img>`;
-                    
+const researchUser = () => {
+    const text = textarea.value;
+    //Nettoyer les résultats précédents
+    resultsContainer.innerHTML = ''; 
 
-                break;
-                } catch (error) {
-                    console.error('Error fetching user information:', error);
-                }
-            }
+    //Si le texte recherché est un psudo Github, on affiche l'username et son avatar
+    for (const username in userInformation) {
+        if (username.includes(text)) {
+            const user = userInformation[username];
+            const userInfo = document.createElement('div');
+            userInfo.innerHTML = `
+                <h1>${username}</h1>
+                <img src="${user.avatar_url}" alt="${username}'s avatar">
+            `;
+            resultsContainer.appendChild(userInfo);
         }
     }
-    
+};
 
+//Le fait de cliquer sur le bouton de recherche affiche la fonction de recherche de l'utilisateur
 researchButton.addEventListener('click', researchUser);
 
+fetchAllUserInformation()
+    .then(() => {
+        console.log(userInformation);
+    })
+    .catch(error => console.error('Error fetching user information:', error));
